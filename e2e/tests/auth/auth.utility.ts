ffa4425e82e8fb713d7c0b7a6103ecd528282bbb
Ibrahim Dembele <idembele70@gmail.com>
2026-01-26 17:05:04 +0100

    [#2] Add register E2E UI tests (#5)
    * test(register): add register e2e ui tests
    
    Refs: #2
    
    * test(auth): changes after review
    
    Refs: #2
    
    ---------
    
    Co-authored-by: idembele70 <idembele70@gmail>

--- e2e/tests/auth/auth.utility.ts
+++ e2e/tests/auth/auth.utility.ts
@@ -1,15 +1,10 @@
-import { request } from "@playwright/test";
+import { APIRequestContext, request } from "@playwright/test";
 import { User } from "./auth.model";
 import { ENV, ENV_CONFIG } from "@config/env.config";
 
 export class AuthUtility {
   static async createAccount(user: User): Promise<string | undefined> {
-    const apiContext = await request.newContext({
-      baseURL: ENV_CONFIG[ENV].baseURL.api,
-      extraHTTPHeaders: {
-        'Content-Type': 'application/json',
-      },
-    });
+    const apiContext = await this.createApiContext();
 
     const response = await apiContext.post('users', {
       data: { user },
@@ -26,13 +21,7 @@ export class AuthUtility {
   }
 
   static async deleteAccount(token: string): Promise<void> {
-    const apiContext = await request.newContext({
-      baseURL: ENV_CONFIG[ENV].baseURL.api,
-      extraHTTPHeaders: {
-        'Content-Type': 'application/json',
-        'Authorization': `Token ${token}`
-      },
-    });
+    const apiContext = await this.createApiContext(token);
 
     const response = await apiContext.delete('user');
 
@@ -41,5 +30,46 @@ export class AuthUtility {
     }
 
     await apiContext.dispose();
+  }
+
+  static async deleteUser(user: User): Promise<void> {
+    const token = await AuthUtility.login(user);
+    await AuthUtility.deleteAccount(token);
+  }
+
+  static async login(user: User): Promise<string> {
+    const apiContext = await this.createApiContext();
+
+    const response = await apiContext.post('users/login', {
+      data: { user },
+    });
+
+    if (!response.ok()) {
+      throw new Error(`Login failed: ${response.status()}`);
+    }
+
+    const token = (await response.json())?.user?.token;
+    await apiContext.dispose();
+
+    return token;
+  }
+
+  static async doesUserExists(user: User): Promise<boolean> {
+    try {
+      const token = await this.login(user);
+      return !!token;
+    } catch {
+      return false;
+    }
+  }
+
+  private static async createApiContext(token?: string): Promise<APIRequestContext> {
+    return await request.newContext({
+      baseURL: ENV_CONFIG[ENV].baseURL.api,
+      extraHTTPHeaders: {
+        'Content-Type': 'application/json',
+        ...(token ? { 'Authorization': `Token ${token}` } : {}),
+      },
+    });
   }
 }
--- e2e/tests/auth/fixtures/login.fixture.ts
+++ e2e/tests/auth/fixtures/login.fixture.ts
@@ -1,18 +1,21 @@
 import { AuthFactory } from '@auth/auth.factory';
 import { User } from '@auth/auth.model';
 import { AuthUtility } from '@auth/auth.utility';
-import { LoginPage } from '@auth/login.page';
+import { LoginPage } from '@auth/pages/login.page';
 import { test as baseTest } from '@playwright/test';
 
-export const loginTest = baseTest.extend<{ loginPage: LoginPage; userOverrides?: Partial<User>; user: User }>({
-  userOverrides: [undefined, { scope: 'test'}],
-  user: [async ({ userOverrides }, use, workerInfo) => {
-    const id = AuthFactory.buildId('login-test', workerInfo.parallelIndex);
-    const baseUser = AuthFactory.buildUser(id);
-    const user = { ...baseUser, ...userOverrides };
+export const loginTest = baseTest.extend<{ loginPage: LoginPage; existingUser: User; newUser: User; }>({
+  existingUser: [async ({}, use, workerInfo) => {
+    const id = AuthFactory.buildId('login-test-existing', workerInfo.parallelIndex);
+    const user = AuthFactory.buildUser(id);
     const token = await AuthUtility.createAccount(user);
     await use(user);
     await AuthUtility.deleteAccount(token);
+  }, { scope: 'test' }],
+  newUser: [async ({}, use, workerInfo) => {
+    const id = AuthFactory.buildId('login-test-new', workerInfo.parallelIndex);
+    const user = AuthFactory.buildUser(id);
+    await use(user);
   }, { scope: 'test' }],
   loginPage: async ({ page }, use) => {
     const loginPage = new LoginPage(page);
@@ -20,4 +23,5 @@ export const loginTest = baseTest.extend<{ loginPage: LoginPage; userOverrides?:
     await use(loginPage);
   }
 });
+
 export const expect = loginTest.expect;
