833efb325c18e27867568e83a33d205b01ef264e
idembele70 <idembele70@gmail>
2026-03-10 14:00:08 +0100

    chore(config): retain trace only on first failure
    Refs: #10

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: ENV_CONFIG[ENV].baseURL.front,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: process.env.CI ? 'retain-on-first-failure' : 'off',
     headless: true,
   },
   /* Configure projects for major browsers */
