a236a831f01c07008641d0e192336260493a285a
idembele70 <idembele70@gmail>
2026-03-11 15:59:40 +0100

    refactor(e2e): format spacing in ARTICLE_EDITOR regex function
    Refs: #11

--- tests/shared/utilities/url-front.utility.ts
+++ tests/shared/utilities/url-front.utility.ts
@@ -6,7 +6,7 @@ export const FRONT_URLS_REG_EXP = {
   HOME: new RegExp(`^${ESCAPED_FRONT_BASE_URL}$`),
   LOGIN: new RegExp(`^${ESCAPED_FRONT_BASE_URL}login$`),
   REGISTER: new RegExp(`^${ESCAPED_FRONT_BASE_URL}register$`),
-  ARTICLE_EDITOR: (slug?:string) => new RegExp(`^${ESCAPED_FRONT_BASE_URL}editor${slug ? `/${slug}` : ''}$`),
+  ARTICLE_EDITOR: (slug?: string) => new RegExp(`^${ESCAPED_FRONT_BASE_URL}editor${slug ? `/${slug}` : ''}$`),
   ARTICLE_DETAILS: (slug?: string) => new RegExp(`^${ESCAPED_FRONT_BASE_URL}article${slug ? `/${slug}` : ''}$`),
 } as const;
 
